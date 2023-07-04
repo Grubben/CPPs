9ffe99ce630db06a39fb4047181576ae6386fd38
Grubben <antoniomsprc@gmail.com>
2023-07-04 14:54:48 +0100

    ex02: finished

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -12,18 +12,65 @@ Base* generate(void)
 	switch (rand123(mt))
 	{
 	case 1:
+		std::cout << "Generating A" << std::endl;
 		return new A;
 	case 2:
+		std::cout << "Generating B" << std::endl;
 		return new B;
 	default:
+		std::cout << "Generating C" << std::endl;
 		return new C;
 	}
+}
+
+void	identify(Base* p)
+{
+	if (dynamic_cast<A*>(p) != nullptr)
+		std::cout << "Identified as A*";
+	else if (dynamic_cast<B*>(p) != nullptr)
+		std::cout << "Identified as B*";
+	else if (dynamic_cast<C*>(p) != nullptr)
+		std::cout << "Identified as C*";
+	else
+		std::cout << "ERROR";
+	std::cout << std::endl;
+}
+
+void	identify(Base& p)
+{
+	try
+	{
+		(void)dynamic_cast<A&>(p);
+		std::cout << "Identified as A&" << std::endl;
+		return;
+	}
+	catch(const std::bad_cast& e)
+	{}
+	try
+	{
+		(void)dynamic_cast<B&>(p);
+		std::cout << "Identified as B&" << std::endl;
+		return;
+	}
+	catch(const std::bad_cast& e)
+	{}
+	try
+	{
+		(void)dynamic_cast<C&>(p);
+		std::cout << "Identified as C&" << std::endl;
+		return;
+	}
+	catch(const std::bad_cast& e)
+	{}
+	
 }
 
 int	main(void)
 {
 	Base*	myst = generate();
 
+	identify(myst);
+	identify(*myst);
 
 
 	delete myst;
