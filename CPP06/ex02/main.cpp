1584d5c5cf52e8e41bfefce38dc66be084b7fa48
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r9s1.42lisboa.com>
2023-10-02 17:29:58 +0100

    c++98 bad random

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
-#include <random>
+#include <cstdlib>
 #include "ABC.hpp"
 
 Base* generate(void)
 {
 	/*Overkill on the random but it's correct*/
-	std::random_device	rd;
-	std::mt19937 mt(rd());
-	std::uniform_int_distribution<int> rand123(1, 3);
+	// std::random_device	rd;
+	// std::mt19937 mt(rd());
+	// std::uniform_int_distribution<int> rand123(1, 3);
 
-	switch (rand123(mt))
+	// switch (rand123(mt))
+	int r = std::rand() % 3;
+	std::cout << r << std::endl;
+	switch (r)
 	{
-	case 1:
+	case 0:
 		std::cout << "Generating A" << std::endl;
 		return new A;
-	case 2:
+	case 1:
 		std::cout << "Generating B" << std::endl;
 		return new B;
 	default:
@@ -25,11 +28,11 @@ Base* generate(void)
 
 void	identify(Base* p)
 {
-	if (dynamic_cast<A*>(p) != nullptr)
+	if (dynamic_cast<A*>(p) != NULL)
 		std::cout << "Identified as A*";
-	else if (dynamic_cast<B*>(p) != nullptr)
+	else if (dynamic_cast<B*>(p) != NULL)
 		std::cout << "Identified as B*";
-	else if (dynamic_cast<C*>(p) != nullptr)
+	else if (dynamic_cast<C*>(p) != NULL)
 		std::cout << "Identified as C*";
 	else
 		std::cout << "ERROR";
@@ -44,7 +47,7 @@ void	identify(Base& p)
 		std::cout << "Identified as A&" << std::endl;
 		return;
 	}
-	catch(const std::bad_cast& e)
+	catch(const std::exception& e)
 	{}
 	try
 	{
@@ -52,7 +55,7 @@ void	identify(Base& p)
 		std::cout << "Identified as B&" << std::endl;
 		return;
 	}
-	catch(const std::bad_cast& e)
+	catch(const std::exception& e)
 	{}
 	try
 	{
@@ -60,13 +63,14 @@ void	identify(Base& p)
 		std::cout << "Identified as C&" << std::endl;
 		return;
 	}
-	catch(const std::bad_cast& e)
+	catch(const std::exception& e)
 	{}
 	
 }
 
 int	main(void)
 {
+	srand(time(NULL));
 	Base*	myst = generate();
 
 	identify(myst);
